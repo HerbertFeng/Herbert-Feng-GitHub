4252dd455ac00a245e65d97cbe01ed91b083b0db
HerbertFeng <herbert.feng.new@gmail.com>
2023-12-12 11:34:29 +0800

    add

--- USACO/Introduction/gift1.py
+++ USACO/Introduction/gift1.py
@@ -8,11 +8,9 @@ TASK: gift1
 def main():
     with open('gift1.in', 'r') as fr, open('gift1.out', 'w') as fw:
         lines = [line.rstrip('\n') for line in fr]
+        times = lines[0]
+        for name in range(1,times+1):
+            name = 0
 
-        for line in lines:
-            fw.write(line+ '\n' )
-    '''
-    for i in range(time):
-        lines[i+1]=0
-    '''
+
 main()
