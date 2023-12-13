1aeb01b6632c817ea9a1ad5d2ad09f94c58f97e2
HerbertFeng <herbert.feng.new@gmail.com>
2023-12-13 20:13:28 +0800

    add USACO

--- USACO/USACO 2023 February Contest, Bronze Problem/1.Hungry_Cow.py
+++ USACO/USACO 2023 February Contest, Bronze Problem/1.Hungry_Cow.py
@@ -5,14 +5,17 @@
 @Date : 12/12/2023 11:35 AM
 '''
 
+'''
 lines, days = map(int, input().split())
 
-count = 0
+
 dic = {}
 for _ in range(lines):
     k, v = map(int, input().split())
     if k <= days:
         dic[k] = v
+        
+count = 0
 stock = 0
 ans = 0
 while count <= days:
@@ -23,3 +26,17 @@ while count <= days:
         ans += 1
     count += 1
 print(ans)
+
+'''
+
+N, T = map(int, input().split())
+deliveries = [tuple(map(int, input().split())) for _ in range(N)] + [(T + 1, 0)]
+
+remaining, total, last_d = 0, 0, 0
+for d, b in deliveries:
+    total += b
+    remaining -= d - last_d
+    last_d = d
+    remaining = max(remaining, 0) + b
+
+print(total - remaining)
