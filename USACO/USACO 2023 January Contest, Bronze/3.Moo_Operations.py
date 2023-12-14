8a1389e12162b4043d0005d0cf09b27e5937d03f
HerbertFeng <herbert.feng.new@gmail.com>
2023-12-14 12:28:22 +0800

    add USACO

--- USACO/USACO 2023 January Contest, Bronze/3.Moo_Operations.py
+++ USACO/USACO 2023 January Contest, Bronze/3.Moo_Operations.py
@@ -4,3 +4,16 @@
 @Author : Herbert
 @Date : 12/14/2023 12:24 PM
 '''
+
+
+def check(word):
+    ans = 0
+    if word[0] != 'M':
+        ans += 1
+    if word[1] != 'O':
+        ans += 1
+    if word[2] != 'O':
+        ans += 1
+    return ans
+
+
